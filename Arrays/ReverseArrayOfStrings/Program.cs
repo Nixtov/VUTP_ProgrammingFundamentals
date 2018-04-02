d058c3fd7c3bb551ac6463ad1db99ffd10399c7a
Nixtov <nixtov@gmail.com>
2018-04-02 23:09:48 +0300

    1

---
+++ Arrays/ReverseArrayOfStrings/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReverseArrayOfStrings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split (' ').ToArray();
+            for (int i = 0; i < input.Length; i++)
+            {
+                Swap (input, i, input.Length -1 -i)
+            }
+        }
+        private static void Swap (string [] input, int i, int v)
+        {
+            string temp = input[i];
+            input[i] = input[v];
+            input[v] = temp;
+        }
+    }
+}
---
+++ Arrays/SumArrays/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SumArrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int [] arr1 = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();        
+            int [] arr2 = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+            int [] result = new int [Math.Max]
+    }
+    }
+}
