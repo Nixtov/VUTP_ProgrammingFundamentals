8eeb12eb9118b737ca5c245c1cd39b9652a87985
Nixtov <nixtov@gmail.com>
2018-04-02 23:04:33 +0300

    EnglishNameOfTheLastDigit

--- MethodsDebuggingAndTroubleshootingCode/EnglishNameOfTheLastDigit/Program.cs
+++ MethodsDebuggingAndTroubleshootingCode/EnglishNameOfTheLastDigit/Program.cs
@@ -18,16 +18,29 @@ namespace EnglishNameOfTheLastDigit
             char LastNumber = number[number.Length - 1];
             switch (LastNumber)
             {
-                case '0': Console.WriteLine("zero"); break;
-                case '1': Console.WriteLine("one"); break;
-                case '2': Console.WriteLine("two"); break;
-                case '3': Console.WriteLine("three"); break;
-                case '4': Console.WriteLine("four"); break;
-                case '5': Console.WriteLine("five"); break;
-                case '6': Console.WriteLine("six"); break;
-                case '7': Console.WriteLine("sevet"); break;
-                case '8': Console.WriteLine("eigth"); break;
-                case '9': Console.WriteLine("nine"); break;
+                case '0': Console.WriteLine("zero");
+                    break;
+                case '1': Console.WriteLine("one");
+                    break;
+                case '2': Console.WriteLine("two");
+                    break;
+                case '3': Console.WriteLine("three");
+                    break;
+                case '4': Console.WriteLine("four");
+                    break;
+                case '5': Console.WriteLine("five");
+                    break;
+                case '6': Console.WriteLine("six");
+                    break;
+                case '7': Console.WriteLine("sevet");
+                    break;
+                case '8': Console.WriteLine("eigth");
+                    break;
+                case '9': Console.WriteLine("nine");
+                    break;
+                default:
+                    Console.WriteLine("error");
+                    break;
             }
         }
     }
