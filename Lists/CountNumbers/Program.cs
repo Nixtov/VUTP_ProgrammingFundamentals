9375cf945dd3e6ede5d304d22ebc0d08da8c561b
Nixtov <nixtov@gmail.com>
2018-04-03 21:39:29 +0300

    2

---
+++ Lists/CountNumbers/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CountNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var inputNumbers = Console.ReadLine().Split(' ').Select(int.Parse).ToList();
+            int count = 0;
+
+            inputNumbers.Sort();
+
+            for (int i = 1; i < inputNumbers.Count; i++)
+            {
+                if (inputNumbers[i] == inputNumbers[i - 1])
+                {
+                    count++;
+                }
+                else
+                {
+                    Console.WriteLine(inputNumbers[i - 1] + " => " + count);
+                    count = 1;
+                }
+            }
+        }
+    }
+}
---
+++ Lists/SquareNumbers/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SquareNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var square = new List <int>();
+            foreach (var num in nums)
+        }
+    }
+}
