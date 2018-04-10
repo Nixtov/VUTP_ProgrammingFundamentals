0cfc4543681a5b2e82230765d2f5eb032b7977cf
Nixtov <nixtov@gmail.com>
2018-04-10 20:46:42 +0300

    new

---
+++ DictionariesLambdaАndLINQ/CountRealNumbers/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CountRealNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<double> inputNumbers = Console.ReadLine().Split(' ').Select(double.Parse).ToList();
+            Dictionary<double, int> occurrences = new Dictionary<double, int>;
+
+            foreach (var number in inputNumbers)
+            {
+                if (!occurrences.ContainsKey(number))
+                {
+                    if (!occurrences.ContainsKey(number))
+                    
+                        [number] = 0;
+                    occurrences[number] = occurrences[number] + 1;
+            }
+                foreach (KeyValuePair<double, int> numberAndCount in occurrences)
+                {
+                    Console.WriteLine($"{numberAndCount.Key}->{numberAndCount.Value}");
+                }
+        }
+    }
+}
---
+++ DictionariesLambdaАndLINQ/OddOccurrences/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OddOccurrences
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Dictionary<string, string> phonebook = new Dictionary<string, string>();
+            //phonebook["Ivan Georgiev"] = "0989 765 843";
+            //phonebook["Pesho Georgiev"] = "0989 888 843";
+            //phonebook["Gosho Georgiev"] = "0989 654 843";
+            //phonebook["Ivan Petrov"] = "0989 455 843";
+            //phonebook.Add("Penka", "34920394-0");
+            //Console.WriteLine(phonebook ["Penka"]);
+
+            List<string> words = Console.ReadLine().Split(' ').ToList();
+            Dictionary<string, int> wordsCount = new Dictionary<string, int>();
+            foreach (var word in words)
+            {
+            if (!wordsCount.ContainsKey(word.ToLower()))
+                {
+                    wordsCount[word.ToLower()] = 0;
+                }
+            wordsCount
+            }
+
+        }
+    }
+}
