699a6d1c8d23515f7838c1dbd725a6db83f7304e
Nixtov <nixtov@gmail.com>
2018-04-12 21:19:05 +0300

    2

---
+++ ObjectsAndClasses/Class1.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+public class Point
+{
+    public Point()
+    {
+
+    public int x { get; set; }
+    public int y { get; set; }
+}
+}
---
+++ ObjectsAndClasses/DistanceBetweenPoints/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DistanceBetweenPoints
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] firstPointInfo = Console.ReadLine()
+                .Split(' ').Select(double.Parse).ToArray();
+        }
+    }
+}
