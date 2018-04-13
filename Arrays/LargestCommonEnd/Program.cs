d9c1b9cb8d6cb3116a733fea6c19c6f7bfa52a09
Nixtov <nixtov@gmail.com>
2018-04-13 23:48:16 +0300

    1

--- Arrays/LargestCommonEnd/Program.cs
+++ Arrays/LargestCommonEnd/Program.cs
@@ -10,6 +10,26 @@ namespace LargestCommonEnd
     {
         static void Main(string[] args)
         {
+
+            string[] first = Console.ReadLine().Split();
+            string[] secound = Console.ReadLine().Split();
+            int leftcounter = 0;
+            int rightcounter = 0;
+
+            int smallLenght = Math.Min(first.Length, secound.Length);
+
+            for (int i = 0; i < smallLenght; i++)
+            {
+                if (first[i] == secound[i])
+                {
+                    leftcounter++;
+                }
+                if (first[first.Length - 1 - i] == secound[secound.Length - 1 - i])
+                {
+                    rightcounter++;
+                }
+            }
+            Console.WriteLine(Math.Max(leftcounter, rightcounter));
         }
     }
 }
