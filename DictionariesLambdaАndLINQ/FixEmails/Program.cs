d37a34fd3a77af5489260e49fd3666205178d17d
Nixtov <nixtov@gmail.com>
2018-04-15 12:21:15 +0300

    1

---
+++ DictionariesLambdaАndLINQ/FixEmails/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FixEmails
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var emails = new Dictionary<string, string>();
+
+        }
+    }
+}
---
+++ DictionariesLambdaАndLINQ/PhonebookUpgrade/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhonebookUpgrade
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var phonebook = new Dictionary<string, string>();//name -> phonenumber
+            string command = Console.ReadLine();
+
+            while (command != "END")
+            {
+                var argms = command.Split(' ').ToArray();
+
+                switch (argms [0])
+                {
+                    default:
+                    case "A":
+                        phonebook[argms[1]] = argms[2];
+                        break;
+                    case "S":
+                        if (phonebook.ContainsKey(argms[1]))
+                        {
+                            Console.WriteLine($"Contact {argms[1]} does not exist");
+                        }
+                        break;
+                    case "ListAll":
+                        foreach (var contact in collection)
+                        {
+
+                        }
+                }
+            }
+        }
+    }
+}
