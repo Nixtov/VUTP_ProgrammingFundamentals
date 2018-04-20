bab0a7718a6a5518741d057fc927ee7cbe409621
Nixtov <nixtov@gmail.com>
2018-04-20 23:55:19 +0300

    q

--- RegEx/MatchFullName/Program.cs
+++ RegEx/MatchFullName/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace MatchFullName
@@ -10,6 +11,14 @@ namespace MatchFullName
     {
         static void Main(string[] args)
         {
+            string regex = @"\b([A-Z][a-z]+ [A-Z][a-z]+)\b";
+            string names = Console.ReadLine();
+            MatchCollection matchedNames = Regex.Matches(names, regex);
+            foreach (Match name in matchedNames)
+            {
+                Console.WriteLine(name.Value + " ");
+            }
+            Console.WriteLine();
         }
     }
 }
