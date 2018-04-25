4f34c68dce598486a064cae8000e801339f9b087
Nixtov <nixtov@gmail.com>
2018-04-25 23:41:14 +0300

    arr6

--- Arrays/CompareCharArray/Program.cs
+++ Arrays/CompareCharArray/Program.cs
@@ -10,7 +10,35 @@ namespace CompareCharArray
     {
         static void Main(string[] args)
         {
+            string[] n = Console.ReadLine().Split(' ').ToArray();
+            string[] k = Console.ReadLine().Split(' ').ToArray();
 
+            if (n.Length <= k.Length && n[0] == "a")
+            {
+                for (int i = 0; i < n.Length; i++)
+                {
+                    Console.Write(n[i]);
+                }
+                Console.WriteLine();
+                for (int i = 0; i < k.Length; i++)
+                {
+                    Console.Write(k[i]);
+                }
+                Console.WriteLine();
+            }
+            else
+            {
+                for (int i = 0; i < k.Length; i++)
+                {
+                    Console.Write(k[i]);
+                }
+                Console.WriteLine();
+                for (int i = 0; i < n.Length; i++)
+                {
+                    Console.Write(n[i]);
+                }
+                Console.WriteLine();
+            }
         }
     }
 }
