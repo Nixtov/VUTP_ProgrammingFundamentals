09b5cda505e981740f53a0309c6e443eb3508639
Nixtov <nixtov@gmail.com>
2018-04-26 19:52:47 +0300

    l

---
+++ Lists/SearchForANumber/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SearchForANumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> lineOfDigits = Console.ReadLine().Split().Select(int.Parse).ToList();
+            List<int> lineOfCommands = Console.ReadLine().Split().Select(int.Parse).ToList();
+
+            int takeThisMuchDigits = lineOfCommands[0];
+            int removeThisMuchDigits = lineOfCommands[1];
+            int checkThisOut = lineOfCommands[2];
+
+            lineOfDigits.Take(takeThisMuchDigits);
+            foreach (var VARIABLE in lineOfDigits)
+            {
+               // Console.WriteLine(VARIABLE);
+            }
+           // Console.WriteLine();
+
+
+            lineOfDigits.RemoveRange(0, removeThisMuchDigits);
+            foreach (var VARIABLE in lineOfDigits)
+            {
+               // Console.WriteLine(VARIABLE);
+            }
+
+            if (lineOfDigits.Contains(checkThisOut))
+            {
+                Console.WriteLine("YES!");
+            }
+            else
+            {
+                Console.WriteLine("NO!");
+            }
+        }
+    }
+}
