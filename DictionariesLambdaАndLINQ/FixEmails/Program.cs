3c2502a33f46a76168f5288efd1e5f1fc0ce426d
Nixtov <nixtov@gmail.com>
2018-04-26 20:03:10 +0300

    l

--- DictionariesLambdaАndLINQ/FixEmails/Program.cs
+++ DictionariesLambdaАndLINQ/FixEmails/Program.cs
@@ -10,8 +10,31 @@ namespace FixEmails
     {
         static void Main(string[] args)
         {
-            var emails = new Dictionary<string, string>();
+            string name = Console.ReadLine();
+            string[] domain = { "uk", "us" };
+            Dictionary<string, string> nameAndEmails = new Dictionary<string, string>();
 
+            while (!name.Equals("stop"))
+            {
+                string email = Console.ReadLine();
+
+                if (!nameAndEmails.ContainsKey(name))
+                {
+                    nameAndEmails.Add(name, null);
+                }
+
+                nameAndEmails[name] = email;
+                name = Console.ReadLine();
+            }
+            nameAndEmails
+               .Where(kvp => !kvp.Value.EndsWith(domain[0]))
+               .Where(kvp => !kvp.Value.EndsWith(domain[1]))
+               .ToDictionary(x => x.Key, x => x.Value);
+
+            foreach (var key in nameAndEmails)
+            {
+                Console.WriteLine($"{key.Key} -> {key.Value}");
+            }
         }
     }
 }
