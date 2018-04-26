b514818383bca71706400799309ff5446f9a11bc
Nixtov <nixtov@gmail.com>
2018-04-26 19:27:32 +0300

    arr

---
+++ Arrays/MaxSequenceOfEqualElements/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MaxSequenceOfEqualElements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> num = Console.ReadLine().Split(' ').Select(int.Parse).ToList();
+            int maxSeq = 0;
+            int currSeq = 0;
+            int counter = 0;
+            int start = 0;
+            for (int i = 0; i < num.Count - 1; i++)
+            {
+                if (num[i] == num[i + 1])
+                {
+                    currSeq++;
+                    counter++;
+                    if (currSeq > maxSeq)
+                    {
+                        maxSeq = currSeq;
+                        if (counter >= 1)
+                        {
+                            start = num[i];
+                        }
+                    }
+                }
+                else
+                {
+                    counter = 0;
+                    currSeq = 0;
+                }
+
+            }
+            for (int i = 0; i <= maxSeq; i++)
+            {
+                Console.Write("{0} ", start);
+            }
+
+        }
+    }
+}
---
+++ Arrays/MostFrequentNumber/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MostFrequentNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+            int currentNumber = 0;
+            int counter = 1;
+            int maxCounter = 0;
+            int maxNumber = 0;
+
+            for (int i = 0; i < numbers.Length - 1; i++)
+            {
+                currentNumber = numbers[i];
+                for (int j = i + 1; j < numbers.Length; j++)
+                {
+                    if (numbers[j] == currentNumber)
+                    {
+                        counter++;
+                    }
+                }
+
+                if (counter > maxCounter)
+                {
+                    maxCounter = counter;
+                    maxNumber = currentNumber;
+                }
+
+                counter = 1;
+            }
+
+            Console.WriteLine($"{maxNumber}");
+
+        }
+    }
+}
