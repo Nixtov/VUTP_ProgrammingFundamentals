7cd5e04b2ac67d7d0067f35b1a741b0a5f279ec2
Nixtov <nixtov@gmail.com>
2018-05-07 23:52:21 +0300

    q

---
+++ Arrays/MultiplyAnArrayOfDoubles/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultiplyAnArrayOfDoubles
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] array = Console.ReadLine().Split(' ').Select(double.Parse).ToArray();
+            double number = double.Parse(Console.ReadLine());
+
+            foreach (var digit in array)
+            {
+                Console.Write(digit * number + " ");
+            }
+        }
+    }
+}
