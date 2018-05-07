e49c350bf747cc5ba7384bae36d8190c33f900d7
Nixtov <nixtov@gmail.com>
2018-05-08 00:07:16 +0300

    q

--- Arrays/SumArrays/Program.cs
+++ Arrays/SumArrays/Program.cs
@@ -10,10 +10,14 @@ namespace SumArrays
     {
         static void Main(string[] args)
         {
+            int[] arr1 = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+            int[] arr2 = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+            int n = Math.Max(arr1.Length, arr2.Length);
+            int[] sumArr = new int[n];
+            for (int i = 0; i < n; i++)
+                sumArr[i] = arr1[i % arr1.Length] + arr2[i % arr2.Length];
 
-            int [] arr1 = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();        
-            int [] arr2 = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
-            int [] result = new int [Math.Max]
-    }
+            Console.WriteLine(string.Join(" ", sumArr));
+        }
     }
 }
