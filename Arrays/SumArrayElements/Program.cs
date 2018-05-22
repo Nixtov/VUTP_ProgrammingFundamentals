ca4993673bd4b28e338316a39c90cae83486f115
Nixtov <nixtov@gmail.com>
2018-05-22 23:42:30 +0300

    a

---
+++ Arrays/SumArrayElements/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SumArrayElements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+            int sum = 0;
+
+            for (int i = 0; i < number; i++)
+            {
+                sum += int.Parse(Console.ReadLine());
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
