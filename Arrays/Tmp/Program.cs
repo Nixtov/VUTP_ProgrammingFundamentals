d050c0be0ad7a54adc620fdb39dac087028f5859
Nixtov <nixtov@gmail.com>
2018-05-24 22:31:11 +0300

    a

--- Arrays/Tmp/Program.cs
+++ Arrays/Tmp/Program.cs
@@ -10,7 +10,14 @@ namespace Tmp
     {
         static void Main(string[] args)
         {
-           
+            double page = double.Parse(Console.ReadLine());  
+            double cover = double.Parse(Console.ReadLine());  
+            int discount = int.Parse(Console.ReadLine());  
+            double design = double.Parse(Console.ReadLine());  
+            int team = int.Parse(Console.ReadLine());
+            double sum = ((page * 899) + (cover * 2))*1.1;
+                //* (1 - (discount) / 100);// design)*(1 - team / 100);
+            Console.WriteLine(sum);
         }
     }
 }
