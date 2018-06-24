dd125d9ff30e5f90d2b45f15721c7f53ef19e116
Nixtov <nixtov@gmail.com>
2018-06-24 22:14:50 +0300

    a

---
+++ Arrays/OddNumbersAtOddPositions2/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OddNumbersAtOddPositions2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            string[] splitInput = input.Split(' ');
+            int[] array = new int[splitInput.Length];
+
+            InitializeArray(splitInput, array);
+            CountOddNumbersAtOddPositions(array);
+        }
+
+        static void InitializeArray(string[] splitInput, int[] array)
+        {
+            for (int i = 0; i < splitInput.Length; i++)
+            {
+                array[i] = int.Parse(splitInput[i]);
+            }
+        }
+
+        static void CountOddNumbersAtOddPositions(int[] array)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                if ((array[i] % 2 == 1 || array[i] % 2 == -1) && i % 2 == 1)
+                {
+                    Console.WriteLine("Index {0} -> {1}", i, array[i]);
+                }
+            }
+        }
+    }
+}
